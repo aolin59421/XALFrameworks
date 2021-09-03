ca04a94fbc35d62acc2a98e4b72cb9dbf589954a
yh <xuaolin@xuaolindeMac-mini.local>
2021-09-03 15:54:03 +0800

    1

---
+++ XALFramework/XALFrameworks.framework/Headers/XALFrameworks.h
@@ -0,0 +1,41 @@
+//
+//  XALFrameworks.h
+//  XALFrameworks
+//
+//  Created by yh on 2021/9/3.
+//
+
+#import <Foundation/Foundation.h>
+#import <HyphenateChat/HyphenateChat.h>
+#import "EaseIMKit.h"
+#import "EMHeaders.h"
+#import "EMDefines.h"
+#import "HXMsgTools.h"
+#import "XALBaseViewController.h"
+#import "XALBaseView.h"
+#import "XALBaseTableViewCell.h"
+#import "XALBaseCollectionViewCell.h"
+#import "XALWebViewController.h"
+#import "UIView+Category.h"
+#import "UILabel+Size.h"
+#import "MBProgressHUD+Category.h"
+#import "UIViewController+Category.h"
+#import "UIFont+Category.h"
+#import "UIColor+Category.h"
+#import "XALPictureBrowserViewController.h"
+#import "XALPickView.h"
+#import "XALNoDataView.h"
+#import "JhDownProgressView.h"
+#import "CCZSegementController.h"
+#import "UserTools.h"
+#import "HXMsgTools.h"
+#import "XALValidationTools"
+//! Project version number for XALFrameworks.
+FOUNDATION_EXPORT double XALFrameworksVersionNumber;
+
+//! Project version string for XALFrameworks.
+FOUNDATION_EXPORT const unsigned char XALFrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XALFrameworks/PublicHeader.h>
+
+
